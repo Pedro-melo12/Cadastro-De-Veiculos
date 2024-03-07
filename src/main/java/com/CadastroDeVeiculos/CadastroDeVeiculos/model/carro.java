43738e29e867ec48026a11e1e50575e9a94599e4
Pedro De Melo <pedrinhomelo2005@gmail.com>
2024-03-07 17:37:20 -0300

    Adicionando anotação para as variaveis

--- src/main/java/com/CadastroDeVeiculos/CadastroDeVeiculos/model/carro.java
+++ src/main/java/com/CadastroDeVeiculos/CadastroDeVeiculos/model/carro.java
@@ -2,7 +2,16 @@ package com.CadastroDeVeiculos.CadastroDeVeiculos.model;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,11 +23,20 @@ import lombok.NoArgsConstructor;
 
 public class carro {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idcarro;
 
+    @Column(columnDefinition = "TEXT")
     private String modelo;
 
+    @Column(nullable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataDeFabricacao;
+
+    @ManyToOne
+    @JoinColumn(name = "idusuario")
+    private usuario usuario;
 
 
 }
--- src/main/java/com/CadastroDeVeiculos/CadastroDeVeiculos/model/usuario.java
+++ src/main/java/com/CadastroDeVeiculos/CadastroDeVeiculos/model/usuario.java
@@ -2,6 +2,12 @@ package com.CadastroDeVeiculos.CadastroDeVeiculos.model;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,13 +18,20 @@ import lombok.NoArgsConstructor;
 
 public class usuario {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idusuario;
 
+    @Column(columnDefinition = "TEXT")
     private String nome;
 
+    @Column(nullable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate anoDeNascimento;
 
+    @Column(nullable = false, length = 100)
     private String email;
 
+    @Column(nullable = false, length = 20)
     private String senha;
 }
